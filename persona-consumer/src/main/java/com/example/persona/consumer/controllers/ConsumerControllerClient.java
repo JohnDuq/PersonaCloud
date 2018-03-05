471d54a07bce993f51659cfd6709fe23a230f408
INTERGRUPO\Jduquer <jduquer@intergrupo.com>
2018-03-05 12:03:41 -0500

    Ajuste de codigo para balance de carga

--- persona-consumer/src/main/java/com/example/persona/consumer/controllers/ConsumerControllerClient.java
+++ persona-consumer/src/main/java/com/example/persona/consumer/controllers/ConsumerControllerClient.java
@@ -3,26 +3,31 @@ package com.example.persona.consumer.controllers;
 import java.io.IOException;
 //import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.client.ServiceInstance;
 //import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 public class ConsumerControllerClient {
 
-	public void getPersona() throws RestClientException, IOException {
+	public static final String CONSULTAR_PERSONAS = "CONSULTAR_PERSONAS";
 
-		String baseUrl = obtenerUrlRegistradaEurekaBalanceado();
-		baseUrl += "/CONSULTAR_PERSONAS";
+	@RequestMapping(value = CONSULTAR_PERSONAS, method = RequestMethod.GET)
+	public void consultarPersonas() throws RestClientException, IOException {
+
+		String baseUrl = obtenerUrlExplicita();
+		baseUrl += "/" + CONSULTAR_PERSONAS;
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = null;
 		try {
@@ -33,14 +38,19 @@ public class ConsumerControllerClient {
 		System.out.println(response.getBody());
 	}
 
-	// public String obtenerUrlExplicita() {
-	// String baseUrl = "http://localhost:8080";
-	// return baseUrl;
-	// }
+	/*
+	 * METODO PARA OBTENER URL DIRECTA
+	 */
+	public String obtenerUrlExplicita() {
+		String baseUrl = "http://localhost:8080";
+		return baseUrl;
+	}
 
+	/*
+	 * METODO PARA OBTENER MICROSERVICIO REGISTRADO
+	 */
 	// @Autowired
 	// private DiscoveryClient discoveryClient;
-	//
 	// public String obtenerUrlRegistradaEureka() {
 	// List<ServiceInstance> instances =
 	// discoveryClient.getInstances("persona-producer");
@@ -49,15 +59,19 @@ public class ConsumerControllerClient {
 	// return baseUrl;
 	// }
 	//
-	@Autowired
-	private LoadBalancerClient loadBalancer;
 
-	public String obtenerUrlRegistradaEurekaBalanceado() {
-		ServiceInstance serviceInstance = loadBalancer.choose("persona-producer");
-		System.out.println(serviceInstance.getUri());
-		String baseUrl = serviceInstance.getUri().toString();
-		return baseUrl;
-	}
+	/*
+	 * METODO PARA OBTENER MICROSERVICIO BALANCEADO
+	 */
+	// @Autowired
+	// private LoadBalancerClient loadBalancer;
+	// public String obtenerUrlRegistradaEurekaBalanceado() {
+	// ServiceInstance serviceInstance =
+	// loadBalancer.choose("persona-producer");
+	// System.out.println(serviceInstance.getUri());
+	// String baseUrl = serviceInstance.getUri().toString();
+	// return baseUrl;
+	// }
 
 	private static HttpEntity<?> getHeaders() throws IOException {
 		HttpHeaders headers = new HttpHeaders();
