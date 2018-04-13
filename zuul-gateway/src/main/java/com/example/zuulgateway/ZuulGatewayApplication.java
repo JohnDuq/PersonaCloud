54035c23d71014a214c130c219dbd6cac0c5832c
INTERGRUPO\Jduquer <jduquer@intergrupo.com>
2018-04-13 11:21:33 -0500

    Se elimina el proxy del registro de eureka

--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -2,7 +2,6 @@ package com.example.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -12,7 +11,6 @@ import com.example.zuulgateway.filter.PreFilter;
 import com.example.zuulgateway.filter.RouteFilter;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulGatewayApplication {
 
